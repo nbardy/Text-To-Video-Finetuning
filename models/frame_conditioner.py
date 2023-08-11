1d08b5d31e12faa636929719f376768af79fcf52
Nicholas Bardy <nicholasbardy@gmail.com>
2023-08-10 18:02:30 -0700

    Update frame_conditioner.py

--- models/frame_conditioner.py
+++ models/frame_conditioner.py
@@ -5,13 +5,16 @@ import torch
 
 # Cross attention layer for computing attention across x and y
 class CrossAttention(nn.Module):
+    # TODO: Test scale
     def __init__(self, dim=512, cross_dim=512, num_heads=8, dropout_p=0.0):
         super().__init__()
 
+        self.scale = dim_head ** -0.5
+
         # Layers for projecting Q, K, V
         self.q_proj = nn.Linear(dim, cross_dim * num_heads)
-        self.k_proj = nn.Linear(dim, cross_dim * num_heads)
-        self.v_proj = nn.Linear(dim, cross_dim * num_heads)
+        self.k_proj = nn.Linear(cross_dim, cross_dim * num_heads)
+        self.v_proj = nn.Linear(cross_dim, cross_dim * num_heads)
 
         self.num_heads = num_heads
         self.cross_dim = cross_dim
@@ -37,7 +40,7 @@ class CrossAttention(nn.Module):
             k_proj,
             v_proj,
             dropout_p=self.dropout_p,
-            softmax_scale=1 / (self.cross_dim**0.5),
+            softmax_scale=1 / (self.scale),
         )
 
         return out  # Output shape: (batch_size, seqlen, nheads, headdim)
@@ -51,7 +54,7 @@ class CrossAttentionBlock(nn.Module):
     def __init__(
         self,
         dim_x=512,
-        dim_y=256,
+        dim_y=512,
         cross_dim=256,
         num_heads=8,
         mlp_hidden_dim=512 * 4,
@@ -76,9 +79,8 @@ class CrossAttentionBlock(nn.Module):
         )
 
     def forward(self, x, y):
-        # Inputs:
-        # x shape: (batch_size, seqlen_x, dim_x)
-        # y shape: (batch_size, seqlen_y, dim_y)
+        # x shape: (batch_size, seqlen_x, dim)
+        # y shape: (batch_size, seqlen_y, dim)
 
         # Layer Norm -> Cross Attention
         attn_out_x = self.cross_attention(
@@ -97,15 +99,35 @@ class CrossAttentionBlock(nn.Module):
 CLIP_EMBED_DIM = 512
 
 
-# Adds two CrossAttentionBlocks to optionally conditon the video on initial and end frames
-class FrameConditionEmbedding(nn.Module):
+# Instead of training on Clip Embeddings directly we will
+# train a small model to project a set of clip embeddings to a
+# video embedding
+#
+# We do this we make a model that takes 3 inputs all optional
+#  1. Base Frame (Optional)
+#  2. Init Frame (optional)
+#  3. Final Frame (Optional)
+# And returns one output:
+#  1. Video Embeddding
+#
+# This accomplishes two things: 
+# First, we can condition the result at inference time on more user input types, allowing greater control
+# 
+# Second, we can fix the mismatch between the prior and output spaces. 
+#
+# DaLLE  showed a mismatch between text and image embedding space causes issues with quality,
+# and fixed this with a projection from text to image space. For our case we use a similar shallow model.
+# (2 Transformer blocks) and we extend this to the video case by projecting sets of temporaly contrasting image embeddings.
+# to find a subspace of video embeddings.
+class CLIP2VidMiniformer(nn.Module):
     def __init__(
         self,
         dim=512,
-        cross_dim=256,
+        cross_dim=512,
         num_heads=8,
         droput_cross_attention=0.0,
         mlp_hidden_dim=1024,
+        mix=False,
     ):
         super().__init__()
 
@@ -123,6 +145,7 @@ class FrameConditionEmbedding(nn.Module):
             dropout_p=droput_cross_attention,
             mlp_hidden_dim=mlp_hidden_dim,
         )
+        self.mix = mix
 
     def forward(self, base_clip_embed=None, initial_clip_embed=None, final_clip_embed=None):
         # default args
@@ -135,9 +158,18 @@ class FrameConditionEmbedding(nn.Module):
         if final_clip_embed is not None:
             final_clip_embed = torch.zeros(clip_embed_dim=CLIP_EMBED_DIM)
 
-        # compute layer graph
         x = base_clip_embed
-        y = self.initial_cross_attention(x, initial_clip_embed)
-        y = self.final_cross_attention(x, final_clip_embed)
+        c1 = initial_clip_embed
+        c2 = final_clip_embed
+        
+        # compute layer graph
+        if self.mix is True: # add
+            y1 = self.initial_cross_attention(x, c1)
+            y2 = self.final_cross_attention(x, c2)
 
-        return x
+            y = y1 + y2
+        else: # seq
+            y = self.initial_cross_attention(x, c1)
+            y = self.final_cross_attention(y, c2)       
+
+        return y
